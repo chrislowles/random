616be2bc2e17f250bf973f9e99d9c7fe89daab08
chrishazfun <chrishazfun@users.noreply.github.com>
2021-02-23 21:47:55 +1100

    Update ac2aca.user.js

--- ac2aca.user.js
+++ ac2aca.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         amazon.com to amazon.com.au
 // @namespace    https://chrishaz.fun/
-// @version      1.0
+// @version      1.1
 // @description  basic javascript that redirects amazon.com to amazon.com.au so you don't get your hopes up too quick lol
 // @author       chrishazfun
 // @match        https://*/*
+// @updateURL    https://raw.githubusercontent.com/chrishazfun/stuff/master/ac2aca.user.js
+// @downloadURL  https://raw.githubusercontent.com/chrishazfun/stuff/master/ac2aca.user.js
 // @grant        none
 // ==/UserScript==
 
