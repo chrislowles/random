025b5418607ede1d4e1817229fa65b389a6e8642
chrishazfun <chrishazfun@users.noreply.github.com>
2022-01-01 19:31:36 +1100

    Update qs-json.js

--- qs-json.js
+++ qs-json.js
@@ -1,16 +1,16 @@
 var qs_json, qs_json = {
-    queryString: function(field, prop) {
+	queryString: function(field, prop) {
 		var string = new RegExp("[?&]" + field + "=([^&#]*)", "i").exec(prop);
 		return string ? string[1] : (undefined ? undefined : null);
-    },
-    json: {
-        toQS: function(param1) {
-            return '?' + Object.keys(param1).map(function(key) {
-                return encodeURIComponent(key) + '=' + encodeURIComponent(param1[key]);
-            }).join('&');
-        },
-        parse: function(param1) {
-            return param1.json();
-        }
-    }
+	},
+	json: {
+		toQS: function(param1) {
+			return '?' + Object.keys(param1).map(function(key) {
+				return encodeURIComponent(key) + '=' + encodeURIComponent(param1[key]);
+			}).join('&');
+		},
+		parse: function(param1) {
+			return param1.json();
+		}
+	}
 };
