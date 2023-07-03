55bb506c20cd9800e00b7b27fa4740fae44692e8
chrishazfun <lowleschris@gmail.com>
2023-07-03 13:38:57 +1000

    Update qs-json.js

--- qs-json.js
+++ qs-json.js
@@ -1,16 +1,10 @@
-var qs_json, qs_json = {
-	queryString: function(field, prop) {
-		var string = new RegExp("[?&]" + field + "=([^&#]*)", "i").exec(prop);
-		return string ? string[1] : (undefined ? undefined : null);
-	},
-	json: {
-		toQS: function(param1) {
-			return '?' + Object.keys(param1).map(function(key) {
-				return encodeURIComponent(key) + '=' + encodeURIComponent(param1[key]);
-			}).join('&');
-		},
-		parse: function(param1) {
-			return param1.json();
-		}
-	}
-};
+function QS(field, prop) {
+	var string = new RegExp(`[?&]${field}=([^&#]*)`, "i").exec(prop);
+	return string ? string[1] : (undefined ? undefined : null);
+}
+
+function jsonToQS(param) {
+	return '?' + Object.keys(param).map(function(key) {
+		return `${encodeURIComponent(key)}=${encodeURIComponent(param[key])}`;
+	}).join('&');
+}
