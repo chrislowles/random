2c49bd6093e711d0c4a26d4c50bf58b50c55d04e
chrishazfun <lowleschris@gmail.com>
2023-07-28 02:04:19 +1000

    throwing out scoop, really gonna try and stick with winget as the only pkg manager

--- windows.ps1
+++ windows.ps1
@@ -44,8 +44,8 @@
 
 #>
 
-Write-Output -Message "Importing AppX module for function"
-Import-Module -Name Appx -UseWindowsPowerShell
+# Write-Output -Message "Importing AppX module for function"
+# Import-Module -Name Appx -UseWindowsPowerShell
 
 Write-Output -Message "Removing crud..."
 (
@@ -123,98 +123,7 @@ Start-Process remove-onedrive.ps1
 Write-Output "Disabling Xbox Overlay prompt to avoid issues with opening gaming apps on Windows"
 cmd.exe /c 'REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f'
 
-Write-Output "Disabling automatic driver downloads"
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching\' -Name SearchOrderConfig -Value 3
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata\' -Name PreventDeviceMetadataFromNetwork -Value 1
-$regKey3 = 'HKLM:\SOFTWARE\Microsoft\Windows\WindowsUpdate'
-if (-not(Test-Path -Path $regKey3)) {
-	New-Item -Path $regKey3
-}
-Set-ItemProperty -Path $regKey3 -Name ExcludeWUDriversInQualityUpdate -Value 1
-
-# scoop {
-	if ((Get-ExecutionPolicy -Scope CurrentUser) -notcontains "Unrestricted") {
-	    Write-Output -Message "Execution Policy adjusted for Scoop"
-	    Start-Process -FilePath "PowerShell" -ArgumentList "Set-ExecutionPolicy", "-Scope", "CurrentUser", "-ExecutionPolicy", "Unrestricted", "-Force" -Verb RunAs -Wait
-	    Write-Output "Rerun this script please."
-	    Start-Sleep -Seconds 5
-	    Break
-	}
-	if (!(Get-Command -Name "scoop" -CommandType Application -ErrorAction SilentlyContinue | Out-Null)) {
-	    Write-Output -Message "Getting Scoop"
-		Invoke-RestMethod get.scoop.sh -OutFile 'scoop.ps1'
-		.\scoop -ScoopDir "C:\Users\$env:username\Scoop"
-		Write-Output "Installing Git with Scoop, 7Zip will be installed as a dependency too"
-		scoop install main/git
-	}
-	Write-Output "Adding Scoop buckets"
-	scoop bucket add extras
-	scoop bucket add games
-	scoop bucket add np https://github.com/ScoopInstaller/Nonportable
-	scoop bucket add chf https://github.com/chrishazfun/bucket
-	Write-Output "Installing what we need from Scoop"
-	(
-		"main/sudo",
-		"main/zip",
-		"extras/audacious",
-		"extras/audacity",
-		"extras/bleachbit",
-		"extras/blender",
-		"games/citra",
-		"extras/cpu-z",
-		"games/dolphin",
-		"games/duckstation",
-		"extras/dupeguru",
-		"games/dxvk",
-		"main/ffmpeg",
-		"extras/googlechrome",
-		"chf/gta-iv-downgrader",
-		"extras/handbrake",
-		"extras/homebank",
-		"main/imagemagick",
-		"extras/kdenlive",
-		"extras/mpv",
-		"main/neovim",
-		"extras/nvcleanstall",
-		"extras/obs-studio",
-		"games/pcsx2",
-		"extras/picard",
-		"extras/powertoys",
-		"games/ppsspp",
-		"games/prismlauncher",
-		"extras/qbittorrent",
-		"extras/qutebrowser",
-		"extras/rainmeter",
-		"games/rpcs3",
-		"extras/rufus",
-		"extras/shutter-encoder",
-		"extras/snappy-driver-installer-origin",
-		"games/steam-rom-manager",
-		"extras/upscayl",
-		"extras/ventoy",
-		"extras/vlc",
-		"main/wget",
-		"extras/winaero-tweaker",
-		"games/xemu",
-		"main/yt-dlp",
-		"games/yuzu"
-	) | ForEach-Object {
-		scoop install $_
-	}
-	Write-Output "Seperately installing some packages as admin"
-	(
-		"np/virtualbox-np",
-		"np/icaros-np",
-		"np/equalizer-apo-np"
-	) | ForEach-Object {
-		sudo scoop install $_
-	}
-	Write-Output "Setting Git Credential Manager Core"
-	git config --global credential.helper manager
-	Write-Output "Importing context menus through regedits included with some Scoop packages"
-	reg import "C:\Users\$env:username\Scoop\apps\7zip\current\install-context.reg"
-	reg import "C:\Users\$env:username\Scoop\apps\git\current\install-context.reg"
-# }
+# https://sourceforge.net/projects/equalizerapo/files/1.3/EqualizerAPO64-1.3.exe/download
 
 # winget {
 	if (!(Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe")) {
@@ -225,6 +134,14 @@ Set-ItemProperty -Path $regKey3 -Name ExcludeWUDriversInQualityUpdate -Value 1
 	}
 	Write-Output "Installing Winget packages"
 	(
+		"Microsoft.XboxGamingOverlay_8wekyb3d8bbwe",
+		"Microsoft.Xbox.TCUI_8wekyb3d8bbwe",
+		"gerardog.gsudo",
+		"yt-dlp.yt-dlp",
+		"Xanashi.Icaros",
+		"JernejSimoncic.Wget",
+		"7zip.7zip",
+		"Oracle.VirtualBox",
 		"Python.Python.3.8",
 		"Microsoft.WindowsTerminal",
 		"SublimeHQ.SublimeText.4",
@@ -239,49 +156,62 @@ Set-ItemProperty -Path $regKey3 -Name ExcludeWUDriversInQualityUpdate -Value 1
 		"Logitech.LogiBolt",
 		"Zoom.Zoom",
 		"PrivateInternetAccess.PrivateInternetAccess",
-		"Oracle.JavaRuntimeEnvironment"
+		"Oracle.JavaRuntimeEnvironment",
+		"YuzuEmu.Yuzu.Mainline",
+		"xemu-project.xemu",
+		"VideoLAN.VLC",
+		"Upscayl.Upscayl",
+		"Discord.Discord",
+		"Audacious.MediaPlayer",
+		"Audacity.Audacity",
+		"BleachBit.BleachBit",
+		"BlenderFoundation.Blender",
+		"TechPowerUp.NVCleanstall",
+		"KDE.Kdenlive",
+		# "games/citra",
+		# "extras/cpu-z",
+		"DolphinEmulator.Dolphin",
+		"stenzek.DuckStation",
+		"DupeGuru.DupeGuru",
+		"Gyan.FFmpeg.Shared",
+		"HandBrake.HandBrake",
+		"HomeBank.HomeBank",
+		"ImageMagick.ImageMagick",
+		"OBSProject.OBSStudio",
+		# "games/pcsx2",
+		"MusicBrainz.Picard",
+		"Microsoft.PowerToys",
+		"PPSSPPTeam.PPSSPP",
+		"PrismLauncher.PrismLauncher",
+		"qBittorrent.qBittorrent",
+		# "extras/rainmeter",
+		# "games/rpcs3",
+		"Rufus.Rufus",
+		"SteamGridDB.RomManager"
 	) | ForEach-Object {
-		winget install --id $_ -e -h --override --scope "machine"
+		winget install -e --id gerardog.gsudo --override --scope "machine"
 	}
 # }
 
-Write-Output "Getting various redistributables"
-Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vc_redist.x86.exe" -OutFile "vc_redist.x86.exe"
-Start-Process vc_redist.x86.exe
-
-Write-Output "Downloading and importing reg files to make Dolphin the default file explorer, also one to undo incase you have issues"
-Invoke-WebRequest -Uri "https://chrishaz.fun/fun/dolphinexplore.reg" -OutFile "DolphinExplore.reg"
-Invoke-WebRequest -Uri "https://chrishaz.fun/fun/dolphinexplore-revert.reg" -OutFile "DolphinExplore-Revert.reg"
-reg import "C:\Users\$env:username\DolphinExplore.reg"
-reg import "C:\Users\$env:username\Scoop\apps\prismlauncher\current\install-associations.reg"
-#reg import "C:\Users\$env:username\DolphinExplore-Revert.reg"
-
-Write-Output "Getting FiveM, the better GTA V multiplayer"
-Invoke-WebRequest -Uri "https://runtime.fivem.net/client/FiveM.exe" -OutFile "FiveM.exe"
-Start-Process FiveM.exe
-
-Write-Output "Getting Xbox App"
-Invoke-WebRequest -Uri "https://aka.ms/XboxInstaller" -OutFile "Xbox.exe"
-Start-Process Xbox.exe
-(
-	"Microsoft.Xbox.TCUI_8wekyb3d8bbwe",
-	"Microsoft.XboxGamingOverlay_8wekyb3d8bbwe"
-) | ForEach-Object {
-	winget install --id $_ -e -h --override --scope "machine"
-}
-
-Write-Output "Getting OpenIV"
-Invoke-WebRequest -Uri "https://openiv.com/WebIV/guest.php?get=1" -OutFile "OpenIV.exe"
-Start-Process OpenIV.exe
-
-# kill edge {
-	#Write-Output "Killing Edge"
-	#Invoke-WebRequest -Uri "https://github.com/ShadowWhisperer/Remove-Edge-Chromium/blob/main/Remove-Edge.exe?raw=true" -OutFile "RemoveEdge.exe"
-	#Start-Process RemoveEdge.exe
-	#Invoke-WebRequest -Uri "https://github.com/mrhaydendp/RemoveEdge/raw/main/RemoveEdge.ps1" -OutFile "RemoveEdge.ps1"
-	#sudo .\RemoveEdge.ps1
-	#irm "https://github.com/mrhaydendp/RemoveEdge/raw/main/RemoveEdge.ps1" | iex
+# urls {
+	Write-Output "Installing packages from URLs"
+	(
+		"https://aka.ms/vs/17/release/vc_redist.x86.exe",
+		"https://aka.ms/XboxInstaller",
+		"https://openiv.com/WebIV/guest.php?get=1",
+		# "https://runtime.fivem.net/client/FiveM.exe"
+	) | ForEach-Object {
+		Invoke-WebRequest -Uri $_ -OutFile "pkg.exe"
+		Start-Process pkg.exe
+	}
 # }
+
+# Write-Output "Downloading and importing reg files to make Dolphin the default file explorer, also one to undo incase you have issues"
+# Invoke-WebRequest -Uri "https://chrishaz.fun/fun/dolphinexplore.reg" -OutFile "DolphinExplore.reg"
+# Invoke-WebRequest -Uri "https://chrishaz.fun/fun/dolphinexplore-revert.reg" -OutFile "DolphinExplore-Revert.reg"
+# reg import "C:\Users\$env:username\DolphinExplore.reg"
+# reg import "C:\Users\$env:username\Scoop\apps\prismlauncher\current\install-associations.reg"
+# reg import "C:\Users\$env:username\DolphinExplore-Revert.reg"
 
 Write-Output "Turning hibernation off"
 sudo powercfg /hibernate off
@@ -289,15 +219,5 @@ sudo powercfg /hibernate off
 Write-Output -Message "Finally we're gonna disable some optional features"
 sudo dism /online /disable-feature /featurename:Internet-Explorer-Optional-amd64 /remove /norestart /warningaction
 sudo dism /online /disable-feature /featurename:WindowsMediaPlayer /remove /norestart /warningaction
-
-Write-Output "Preparing for Linux Subsystem, installing Debian"
-sudo Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All -NoRestart
-sudo Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All -NoRestart
-#sudo dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-#sudo dism /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile "wsl.msi"
-Start-Process wsl.msi
-wsl --set-default-version 2
-wsl --install -d Debian # install debian
 
 Write-Output "Done! You should probably reboot before doing anything"
