7b626f3c9dcd142dca5f71fb1b428f558cfd68a3
chrishazfun <lowleschris@gmail.com>
2023-11-04 23:05:36 +1100

    reworked lmde.sh to use proper grammar

--- LMDE.md
+++ LMDE.md
@@ -4,6 +4,8 @@ Since this is the script for my daily OS I thought I'd write more about it, it d
 Make sure the system is completely updated, complete all the tasks you're given in the first Welcome Screen **(Snapshots, Codecs, Update Manager Setup [Sources on Fastest Mirror, Source Code Repos] followed by actual use of Update Manager to update the whole system)**, then use the script after a reboot.
 
 ```bash
-# if you don't have git installed (can't clone repo)
-bash <(curl -s https://github.com/chrishazfun/random/raw/master/script-name.sh)
+# all required
+sudo apt install git
+git clone https://github.com/chrishazfun/random
+sudo bash random/lmde.sh
 ```
--- lmde.sh
+++ lmde.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-	echo "must be run as root"
+	echo "Must be run as root."
 	exit 1
 fi
 
-echo "installing some obvious native pkgs"
-sleep 1
-nativeInstall() {
-	apt install git
-}
-if ! nativeInstall; then
-	echo "failed to install some native pkgs"
-	exit 1
-fi
+#echo "Installing some obvious native packages."
+#sleep 1
+#nativeInstall() {}
+#if ! nativeInstall; then
+#	echo "Failed to install some native packages."
+#	exit 1
+#fi
 
-echo "removing certain default apps"
+echo "Removing certain default apps."
 sleep 1
 removeDefaults() {
 	apt remove hexchat thunderbird hypnotix rhythmbox
 }
 if ! removeDefaults; then
-	echo "failed to remove certain default apps"
+	echo "Failed to remove certain default apps."
 	exit 1
 fi
 
@@ -53,41 +51,40 @@ fi
 #	exit 1
 #fi
 
-echo "adding flathub repo and installing various recommended flatpak packages"
-sleep 1
-flatpakInstall() {
-	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-	#echo "getting third party repo for shutter encoder flatpak"
-	#apt -yy install git
-	#git clone https://github.com/pobthebuilder/shutter-encoder-flatpak
-	#cd "shutter-encoder-flatpak"
-	#flatpak-builder --force-clean --repo=.repo .build-dir org.paulpacifico.ShutterEncoder.yaml
-	#flatpak build-bundle .repo shutter-encoder.flatpak org.paulpacifico.ShutterEncoder
-	#flatpak install shutter-encoder.flatpak
-
-	echo "essentials"; sleep 3
-	flatpak install com.github.tchx84.Flatseal me.timschneeberger.jdsp4linux
-
-	echo "ms stuff"; sleep 3
-	flatpak install com.microsoft.Edge com.visualstudio.code com.github.IsmaelMartinez.teams_for_linux
-
-	echo "gaming"; sleep 3
-	flatpak install uk.co.powdertoy.tpt io.itch.itch com.heroicgameslauncher.hgl
-	echo "steam specifically"; sleep 2
-	flatpak install com.valvesoftware.Steam com.steamgriddb.steam-rom-manager
-	echo "emulators"; sleep 2
-	flatpak install net.rpcs3.RPCS3 org.citra_emu.citra net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP
-
-	echo "production"; sleep 3
-	flatpak install com.obsproject.Studio org.gimp.GIMP org.tenacityaudio.Tenacity org.godotengine.Godot org.kde.kdenlive
-	flatpak install org.ardour.Ardour org.blender.Blender fr.handbrake.ghb
-
-	echo "odds and ends"; sleep 3
-	flatpak install org.bleachbit.BleachBit org.musicbrainz.Picard org.jellyfin.JellyfinServer com.feaneron.Boatswain org.atheme.audacious org.qbittorrent.qBittorrent com.sindresorhus.Caprine com.discordapp.Discord io.github.shiftey.Desktop org.upscayl.Upscayl
+allAtOnce() {
+	flatpak install com.github.tchx84.Flatseal me.timschneeberger.jdsp4linux com.microsoft.Edge com.visualstudio.code com.github.IsmaelMartinez.teams_for_linux uk.co.powdertoy.tpt io.itch.itch com.heroicgameslauncher.hgl com.valvesoftware.Steam com.steamgriddb.steam-rom-manager net.rpcs3.RPCS3 org.citra_emu.citra net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP com.obsproject.Studio org.gimp.GIMP org.tenacityaudio.Tenacity org.godotengine.Godot org.kde.kdenlive org.ardour.Ardour org.blender.Blender fr.handbrake.ghb org.bleachbit.BleachBit org.musicbrainz.Picard org.jellyfin.JellyfinServer com.feaneron.Boatswain org.atheme.audacious org.qbittorrent.qBittorrent com.sindresorhus.Caprine com.discordapp.Discord io.github.shiftey.Desktop org.upscayl.Upscayl
 
 }
-if ! flatpakInstall; then
-	echo "failed to add flathub repo and install various recommended flatpak packages"
-	exit 1
-fi
+
+sectionedInstall() {
+	echo "Essentials"
+	sleep 1
+	flatpak install com.github.tchx84.Flatseal me.timschneeberger.jdsp4linux
+	echo "MS Stuff"
+	sleep 1
+	flatpak install com.microsoft.Edge com.visualstudio.code com.github.IsmaelMartinez.teams_for_linux
+	echo "Gaming"
+	sleep 1
+	flatpak install uk.co.powdertoy.tpt io.itch.itch com.heroicgameslauncher.hgl net.rpcs3.RPCS3 org.citra_emu.citra net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP
+	echo "Steam specifically"
+	sleep 1
+	flatpak install com.valvesoftware.Steam com.steamgriddb.steam-rom-manager
+	echo "Production"
+	sleep 1
+	flatpak install com.obsproject.Studio org.gimp.GIMP org.tenacityaudio.Tenacity org.godotengine.Godot org.kde.kdenlive org.ardour.Ardour org.blender.Blender fr.handbrake.ghb
+	echo "Odds and ends"
+	sleep 1
+	flatpak install org.bleachbit.BleachBit org.musicbrainz.Picard org.jellyfin.JellyfinServer com.feaneron.Boatswain org.atheme.audacious org.qbittorrent.qBittorrent com.sindresorhus.Caprine com.discordapp.Discord io.github.shiftey.Desktop org.upscayl.Upscayl
+}
+
+read -p "Install recommended Flatpaks in sections? (No installs them all at once, for unattended uses) [Y/n] " yn
+sleep 1
+case $yn in
+	y ) sectionedInstall;;
+	n ) allAtOnce;;
+	* ) sectionedInstall;;
+esac
+
+echo "Done!"
