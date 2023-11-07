cb332173453b8a445dd9baa95fe07edacbc14ad6
chrishazfun <lowleschris@gmail.com>
2023-11-08 00:27:00 +1100

    new default apps to remove, no more choice on sectioned install or all in one for flatpaks

--- lmde.sh
+++ lmde.sh
@@ -16,7 +16,7 @@ fi
 echo "Removing certain default apps."
 sleep 1
 removeDefaults() {
-	apt remove hexchat thunderbird hypnotix rhythmbox
+	apt remove hexchat thunderbird hypnotix rhythmbox drawing
 }
 if ! removeDefaults; then
 	echo "Failed to remove certain default apps."
@@ -51,40 +51,15 @@ fi
 #	exit 1
 #fi
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# io.itch.itch com.microsoft.Edge
 
-allAtOnce() {
-	flatpak install com.github.tchx84.Flatseal me.timschneeberger.jdsp4linux com.microsoft.Edge com.visualstudio.code com.github.IsmaelMartinez.teams_for_linux uk.co.powdertoy.tpt io.itch.itch com.heroicgameslauncher.hgl com.valvesoftware.Steam com.steamgriddb.steam-rom-manager net.rpcs3.RPCS3 org.citra_emu.citra net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP com.obsproject.Studio org.gimp.GIMP org.tenacityaudio.Tenacity org.godotengine.Godot org.kde.kdenlive org.ardour.Ardour org.blender.Blender fr.handbrake.ghb org.bleachbit.BleachBit org.musicbrainz.Picard org.jellyfin.JellyfinServer com.feaneron.Boatswain org.atheme.audacious org.qbittorrent.qBittorrent com.sindresorhus.Caprine com.discordapp.Discord io.github.shiftey.Desktop org.upscayl.Upscayl
-
-}
-
-sectionedInstall() {
-	echo "Essentials"
-	sleep 1
-	flatpak install com.github.tchx84.Flatseal me.timschneeberger.jdsp4linux
-	echo "MS Stuff"
-	sleep 1
-	flatpak install com.microsoft.Edge com.visualstudio.code com.github.IsmaelMartinez.teams_for_linux
-	echo "Gaming"
-	sleep 1
-	flatpak install uk.co.powdertoy.tpt io.itch.itch com.heroicgameslauncher.hgl net.rpcs3.RPCS3 org.citra_emu.citra net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP
-	echo "Steam specifically"
-	sleep 1
-	flatpak install com.valvesoftware.Steam com.steamgriddb.steam-rom-manager
-	echo "Production"
-	sleep 1
-	flatpak install com.obsproject.Studio org.gimp.GIMP org.tenacityaudio.Tenacity org.godotengine.Godot org.kde.kdenlive org.ardour.Ardour org.blender.Blender fr.handbrake.ghb
-	echo "Odds and ends"
-	sleep 1
-	flatpak install org.bleachbit.BleachBit org.musicbrainz.Picard org.jellyfin.JellyfinServer com.feaneron.Boatswain org.atheme.audacious org.qbittorrent.qBittorrent com.sindresorhus.Caprine com.discordapp.Discord io.github.shiftey.Desktop org.upscayl.Upscayl
-}
-
-read -p "Install recommended Flatpaks in sections? (No installs them all at once, for unattended uses) [Y/n] " yn
+echo "Installing preferred flatpaks."
 sleep 1
-case $yn in
-	y ) sectionedInstall;;
-	n ) allAtOnce;;
-	* ) sectionedInstall;;
-esac
-
-echo "Done!"
+flatpakInstall() {
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak install com.github.tchx84.Flatseal me.timschneeberger.jdsp4linux com.vscodium.codium uk.co.powdertoy.tpt com.heroicgameslauncher.hgl com.valvesoftware.Steam com.steamgriddb.steam-rom-manager net.rpcs3.RPCS3 org.citra_emu.citra net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP com.obsproject.Studio org.gimp.GIMP org.tenacityaudio.Tenacity org.godotengine.Godot org.kde.kdenlive org.ardour.Ardour org.blender.Blender fr.handbrake.ghb org.bleachbit.BleachBit org.musicbrainz.Picard org.jellyfin.JellyfinServer com.feaneron.Boatswain org.atheme.audacious org.qbittorrent.qBittorrent com.sindresorhus.Caprine com.discordapp.Discord io.github.shiftey.Desktop org.upscayl.Upscayl
+}
+if ! flatpakInstall; then
+	echo "Failed to install preferred flatpaks."
+	exit 1
+fi
