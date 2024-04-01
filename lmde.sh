14716cfb4897d89cc5acaa9c8078e25d88137e43
chrishazfun <lowleschris@gmail.com>
2024-04-02 06:48:37 +1100

    ditched cloudflare warp install in lmde script

--- lmde.sh
+++ lmde.sh
@@ -44,11 +44,6 @@ echo "Installing preferred native pkgs."
 sleep 1
 nativeInstall() {
 	apt install sublime-text kdocker dupeguru
-	# cloudflare warp native install
-	curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-	echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-	apt-get update
-	apt-get install cloudflare-warp
 }
 if ! nativeInstall; then
 	echo "Failed to install preferred native pkgs."
