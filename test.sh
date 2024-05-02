4ecc6e824d3d87986dbdd7c77c8bc8bcd3f567fe
chrishazfun <lowleschris@gmail.com>
2024-05-02 22:53:28 +1000

    throwing a test for downloading nvidia drivers on fedora silverblue

--- test.sh
+++ test.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "response :D"
+echo "nvidia drivers time... yayyyy [test]"
+installNvidiaAction() {
+	sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
+	sudo rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
+}
+installNvidia() {
+	if ! installNvidiaAction; then
+		echo "failed to install nvidia drivers"
+		exit 1
+	fi
+}
+while true; do
+    read -p "$* [y/n]: " yn
+    case $yn in
+        #[Yy]*) installNvidia();  
+        [Yy]*) echo "yes";  
+        [Nn]*) echo "no";
+		*) echo "no response so no drivers";
+    esac
+done
