c80425d85023bc16bb7c466997b0a08fbaf0804b
chrishazfun <lowleschris@gmail.com>
2024-05-05 02:28:22 +1000

    got rid of the dnf stuff and sudo cmds
    the system upgrade can happen seperately

--- sbk.sh
+++ sbk.sh
@@ -5,18 +5,6 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-# dnf config
-echo "updating dnf config"
-sleep 2
-echo -e "deltarpm=True
-fastestmirror=True
-max_parallel_downloads=10
-defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf
-
-echo "upgrading system"
-sleep 2
-rpm-ostree upgrade
-
 echo "install rpmfusion for nvidia drivers, and other stuff"
 sleep 2
 rpm-ostree install \
@@ -43,11 +31,12 @@ fi
 echo "adding flathub and preferred flatpaks"
 sleep 1
 installThings() {
+	# install distrobox for boxbuddy
+	rpm-ostree install distrobox
 	# add regular flathub
-	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 	# install the things
-	sudo flatpak install flathub io.github.dvlv.boxbuddyrs
-	sudo flatpak install flathub com.feaneron.Boatswain com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep org.kde.kdenlive io.freetubeapp.FreeTube com.discordapp.Discord org.qbittorrent.qBittorrent io.mpv.Mpv org.atheme.audacious org.audacityteam.Audacity com.github.qarmin.czkawka org.gimp.GIMP io.github.shiftey.Desktop org.libreoffice.LibreOffice io.github.pwr_solaar.solaar org.nickvision.tagger org.upscayl.Upscayl com.spotify.Client io.github.peazip.PeaZip org.bleachbit.BleachBit fr.handbrake.ghb com.obsproject.Studio us.zoom.Zoom me.timschneeberger.jdsp4linux com.visualstudio.code /
+	flatpak install flathub io.github.dvlv.boxbuddyrs com.feaneron.Boatswain com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep org.kde.kdenlive io.freetubeapp.FreeTube com.discordapp.Discord org.qbittorrent.qBittorrent io.mpv.Mpv org.atheme.audacious org.audacityteam.Audacity com.github.qarmin.czkawka org.gimp.GIMP io.github.shiftey.Desktop org.libreoffice.LibreOffice io.github.pwr_solaar.solaar org.nickvision.tagger org.upscayl.Upscayl com.spotify.Client io.github.peazip.PeaZip org.bleachbit.BleachBit fr.handbrake.ghb com.obsproject.Studio us.zoom.Zoom me.timschneeberger.jdsp4linux com.visualstudio.code \
 	com.valvesoftware.Steam com.steamgriddb.steam-rom-manager com.mojang.Minecraft uk.co.powdertoy.tpt io.itch.itch net.rpcs3.RPCS3 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP net.pcsx2.PCSX2
 }
 if ! installThings; then
