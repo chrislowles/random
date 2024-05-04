5709ef2b75eb46f56ad6816810b29a1ca8f56292
chrishazfun <lowleschris@gmail.com>
2024-05-05 05:40:04 +1000

    printf cmd was a copy-paste, fixed that
    fixed distrobox/boxbuddy install, reworked flatpak installs

--- sbk.sh
+++ sbk.sh
@@ -22,21 +22,25 @@ installNvidia() {
 		exit 1
 	fi
 }
-printf 'Is this a good question (y/N)? '
+printf 'install nvidia drivers? (y/N) '
 read ny
 if [ "$ny" != "${ny#[Yy]}" ]; then
     installNvidia
 fi
 
-echo "adding flathub and preferred flatpaks"
-sleep 1
+echo "adding distrobox/boxbuddy, flathub and preferred flatpaks" && sleep 1
 installThings() {
-	# install distrobox for boxbuddy
-	rpm-ostree install distrobox
-	# add regular flathub
+
+	echo "generic curl cmd for distrobox" && sleep 1
+	curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+	echo "boxbuddy: distrobox gui" && sleep 1
+	flatpak install flathub io.github.dvlv.boxbuddyrs
+
+	echo "adding unfiltered flathub" && sleep 1
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	# install the things
-	flatpak install flathub io.github.dvlv.boxbuddyrs com.feaneron.Boatswain com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep org.kde.kdenlive io.freetubeapp.FreeTube com.discordapp.Discord org.qbittorrent.qBittorrent io.mpv.Mpv org.atheme.audacious org.audacityteam.Audacity com.github.qarmin.czkawka org.gimp.GIMP io.github.shiftey.Desktop org.libreoffice.LibreOffice io.github.pwr_solaar.solaar org.nickvision.tagger org.upscayl.Upscayl com.spotify.Client io.github.peazip.PeaZip org.bleachbit.BleachBit fr.handbrake.ghb com.obsproject.Studio us.zoom.Zoom me.timschneeberger.jdsp4linux com.visualstudio.code \
+
+	echo "installing preferred flatpaks" && sleep 1
+	flatpak install flathub com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep com.feaneron.Boatswain org.kde.kdenlive io.freetubeapp.FreeTube com.discordapp.Discord org.qbittorrent.qBittorrent io.github.celluloid_player.Celluloid org.audacityteam.Audacity org.gimp.GIMP io.github.shiftey.Desktop org.libreoffice.LibreOffice io.github.pwr_solaar.solaar org.nickvision.tagger org.upscayl.Upscayl com.spotify.Client io.github.peazip.PeaZip org.bleachbit.BleachBit fr.handbrake.ghb com.obsproject.Studio us.zoom.Zoom me.timschneeberger.jdsp4linux com.vscodium.codium com.rafaelmardojai.SharePreview \
 	com.valvesoftware.Steam com.steamgriddb.steam-rom-manager com.mojang.Minecraft uk.co.powdertoy.tpt io.itch.itch net.rpcs3.RPCS3 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP net.pcsx2.PCSX2
 }
 if ! installThings; then
