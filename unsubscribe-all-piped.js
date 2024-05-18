f7b204ad01151a96904c3e6323adb3cbba9d0d13
chrishazfun <lowleschris@gmail.com>
2024-05-19 04:24:01 +1000

    piped unsub from all quick script

---
+++ unsubscribe-all-piped.js
@@ -0,0 +1,7 @@
+// unsubscribe from all the channels on a piped account
+// go to subs view on instance
+// use below code
+
+document.querySelectorAll(".w-full.mt-2.btn").forEach(function(a) {
+	a.click();
+});
