ea59685defada9f92001e57bf5d04c444e7e5e24
chrishazfun <lowleschris@gmail.com>
2024-05-21 04:47:49 +1000

    moving boxbuddy install into big flatpak install and more escapes

--- linux.sh
+++ linux.sh
@@ -34,15 +34,15 @@ installThings() {
 	sleep 1
 	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-	echo "generic curl install for distrobox & distrobox gui in boxbuddyrs"
+	echo "generic curl install for distrobox"
 	sleep 1
 	curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
-	sleep 1
-	flatpak install flathub io.github.dvlv.boxbuddyrs
 
-	echo "flatpak permissions and cleaner & other preferred flatpaks"
+	echo "flatpak permissions and cleaner, distrobox gui and other preferred flatpaks"
 	sleep 1
-	flatpak install flathub com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep com.feaneron.Boatswain org.kde.kdenlive io.freetubeapp.FreeTube com.discordapp.Discord org.qbittorrent.qBittorrent io.github.celluloid_player.Celluloid org.audacityteam.Audacity org.gimp.GIMP io.github.shiftey.Desktop org.libreoffice.LibreOffice io.github.pwr_solaar.solaar org.nickvision.tagger org.upscayl.Upscayl com.spotify.Client io.github.peazip.PeaZip org.bleachbit.BleachBit fr.handbrake.ghb com.obsproject.Studio us.zoom.Zoom me.timschneeberger.jdsp4linux com.vscodium.codium com.rafaelmardojai.SharePreview \
+	flatpak install flathub \
+	com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep io.github.dvlv.boxbuddyrs \
+	com.feaneron.Boatswain org.kde.kdenlive io.freetubeapp.FreeTube com.discordapp.Discord org.qbittorrent.qBittorrent io.github.celluloid_player.Celluloid org.audacityteam.Audacity org.gimp.GIMP io.github.shiftey.Desktop org.libreoffice.LibreOffice io.github.pwr_solaar.solaar org.nickvision.tagger org.upscayl.Upscayl com.spotify.Client io.github.peazip.PeaZip org.bleachbit.BleachBit fr.handbrake.ghb com.obsproject.Studio us.zoom.Zoom me.timschneeberger.jdsp4linux com.vscodium.codium com.rafaelmardojai.SharePreview \
 	com.valvesoftware.Steam com.steamgriddb.steam-rom-manager com.mojang.Minecraft uk.co.powdertoy.tpt io.itch.itch net.rpcs3.RPCS3 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP net.pcsx2.PCSX2
 }
 if ! installThings; then
