3c864e6676122a2bf8a3189d3bb5c5241801f999
Chris Lowles <lowleschris@gmail.com>
2025-03-22 01:18:44 +1100

    Delete blocktube-advanced.js

--- blocktube-advanced.js
+++
@@ -1,6 +0,0 @@
-// using regex to filter by title on blocktube
-(a, b) => {
-	if (a.title && a.title.match(/((Huge|Big|Major) (.*) (Drama|Story))/g)) return true;
-	console.log(a, b);
-	return false;
-}
